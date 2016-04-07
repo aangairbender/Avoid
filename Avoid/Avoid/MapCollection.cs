a987f6ae2e98db609b654f973e8aaa825769601a
xanter30 <xanter30@gmail.com>
2016-04-07 17:48:58 +0300

    123

--- Avoid/Avoid/MapCollection.cs
+++ Avoid/Avoid/MapCollection.cs
@@ -8,16 +8,21 @@ namespace Avoid
 {
     class MapCollection
     {
-        bool proc(float x)
+        bool proc(double x)
         {
+            int q = Convert.ToInt32(x*100);
+            Random rnd1 = new Random();
+            int w =rnd1.Next(10000);
+            if(w>q)
             return true;
+            return false;
         }
-        Map generateRandomMap()
+        Map generateRandomMap(int len)
         {
             Random rnd = new Random();
             Map cur = new Map();
-            cur.length = 6000;
-            float chance = 10;
+            cur.length = len;
+            double chance = 10;
             for (int i = 1; i <= cur.length; ++i)
             {
                 for (int j = 0; j < 4; ++j)
@@ -31,9 +36,12 @@ namespace Avoid
                 }
                 if (i - minx < 4)
                 {
+                    int q = rnd.Next(0, 3);
+                    while (cur.v[q].Count() != 0 && cur.v[q].Last() > i - 4) cur.v[q].Remove(cur.v[q].Last());
                     //s
 
                 }
+                chance = chance * 1.01;
             }
                 return cur;
 
@@ -42,10 +50,11 @@ namespace Avoid
         {
             Map test1 = new Map();
             // 1 sec = 100
-            test1.length = 1000;
+            test1.length = 100;
             test1.v[0].Add(1);
             test1.v[1].Add(30);
             test1.v[2].Add(40);
+            Map test2 = generateRandomMap(200);
         }
     }
 }
